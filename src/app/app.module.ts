3983856272f34fa61f06ca09348335010faa0af5
makercode <makercode@gmail.com>
2022-09-01 13:35:19 -0500

    added views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HttpClientModule } from '@angular/common/http';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore'
+import { HttpClientModule } from '@angular/common/http'
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app'
+import { environment } from '../environments/environment'
+import { provideAuth,getAuth } from '@angular/fire/auth'
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './account/components/register/register.component';
+import { LoginComponent } from './account/components/login/login.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AccountComponent,
+    AboutComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
