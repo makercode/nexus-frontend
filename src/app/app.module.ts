55235c7980b782d31a1f1dd85d6a5de42b22338c
cesar calle <makercode@gmail.com>
2022-09-03 15:21:11 -0500

    added material button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { MatToolbarModule,  } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -44,6 +45,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatIconModule,
     BrowserAnimationsModule,
+    MatButtonModule,
 
     // Angular fire
     provideFirebaseApp(() => initializeApp(environment.firebase)),
