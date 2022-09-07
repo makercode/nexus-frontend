10e5720e499754f405c18481ee04b1ba48560e11
makercode <makercode@gmail.com>
2022-09-07 16:37:23 -0500

    added recover view

--- src/app/_views/account/components/register/register.component.ts
+++ src/app/_views/account/components/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from '../../../../_services';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  level = 'weak'
 
-  ngOnInit(): void {
+  constructor(
+    public authService: AuthService,
+    // private alertService: AlertService
+  ) {}
+
+  get username() { return this.registerForm.get('username')!; }
+  get password() { return this.registerForm.get('password')!; }
+
+  registerForm: FormGroup = new FormGroup({
+    username: new FormControl( '', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl( '', [
+      Validators.required,
+      Validators.minLength(9),
+    ]),
+    
+  });
+ 
+  alphaNumberOnly (e:any) {  // Accept only alpha numerics, not special characters 
+    var regex = new RegExp("^[a-zA-Z0-9 ]+$");
+    var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+    if (regex.test(str)) {
+        return true;
+    }
+
+    e.preventDefault();
+    return false;
   }
 
+  onPaste(e:any) {
+    e.preventDefault();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+  signUp( username: string, userpassword: string ) {
+    this.authService.signUp( username, userpassword );
+  }
+
+  googleAuth() {
+    this.authService.googleAuth();
+  }
 }
