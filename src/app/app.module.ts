0a9d7c10df7e43ccb729c819af375db7c71a025b
makercode <makercode@gmail.com>
2022-09-06 19:36:36 -0500

    deleted no sense status services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
+// Angular
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule }   from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { FormsModule }   from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
 import { environment } from '../environments/environment'
-import { provideAuth,getAuth } from '@angular/fire/auth'
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+// Views
+import { AppComponent } from './app.component'
+import { AppRoutingModule } from './app-routing.module'
 import { HomeComponent } from './_views/home/home.component';
 import { AccountComponent } from './_views/account/account.component';
 import { AboutComponent } from './_views/about/about.component';
@@ -17,16 +17,24 @@ import { LoginComponent } from './_views/account/components/login/login.componen
 import { ConfigComponent } from './_views/account/components/config/config.component';
 import { FooterComponent } from './_components/footer/footer.component';
 import { HeaderComponent } from './_components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+// Angular material
 import { MatToolbarModule,  } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar'
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
+// Angular Firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
+import { provideAuth,getAuth } from '@angular/fire/auth'
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import {
   UserService,
@@ -56,6 +64,7 @@ import {
     // Material
     MatSnackBarModule,
     MatFormFieldModule,
+    MatInputModule,
     MatToolbarModule,
     MatIconModule,
     BrowserAnimationsModule,
@@ -64,7 +73,9 @@ import {
     // Angular fire
     AngularFirestoreModule,
     AngularFireAuthModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebase),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
