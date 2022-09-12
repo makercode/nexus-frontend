d43505301eee725facb8bd6bd18e284bb14cc630
makercode <makercode@gmail.com>
2022-09-12 16:01:04 -0500

    fixing project name and configuring firebase deploy

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/abatarify-frontend'),
+      dir: require('path').join(__dirname, './coverage/nexus-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   });
 
-  it(`should have as title 'abatarify-frontend'`, () => {
+  it(`should have as title 'nexus-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.componentInstance
-    expect(app.title).toEqual('abatarify-frontend')
+    expect(app.title).toEqual('nexus-frontend')
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('.content span')?.textContent).toContain('abatarify-frontend app is running!')
+    expect(compiled.querySelector('.content span')?.textContent).toContain('nexus-frontend app is running!')
   });
 });
