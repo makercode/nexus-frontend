c5b66508c2a25c9d41f31758350ed2ab7281e80c
makercode <makercode@gmail.com>
2022-09-20 16:57:19 -0500

    added directives for domains verification

---
+++ src/app/_directives/subdomain.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SubdomainDirective } from './subdomain.directive';
+
+describe('SubdomainDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SubdomainDirective();
+    expect(directive).toBeTruthy();
+  });
+});
